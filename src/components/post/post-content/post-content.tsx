03231e25bf411e12b165f0f066e336d3f3f64c60
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-03-06 13:34:25 +0700

    fix: blockquote color

--- src/components/post/post-content/post-content.tsx
+++ src/components/post/post-content/post-content.tsx
@@ -39,7 +39,7 @@ const CustomBlockquote = (props: JSX.IntrinsicElements["blockquote"]) => (
       <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
     </svg>
     <blockquote
-      className="relative text-lg  p-4 border-l-4 border-primary bg-gray-50  rounded-md transition-all duration-300"
+      className="relative text-lg  p-4 border-l-4 border-primary bg-accent rounded-md transition-all duration-300"
       {...props}
     />
 
