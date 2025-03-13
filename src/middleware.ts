b0db5ace82fd97e9b3649e6916e7f389c75c9aa3
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-03-13 23:11:32 +0700

    feat: update middleware private route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const privatePaths = ['/profile']
+const privatePaths = ['/profile', '/home']
 const authPaths = ['/register', '/login', '/forgot-password']
 
 export function middleware(request: NextRequest) {
