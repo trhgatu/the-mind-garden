a2419acaf5acbec621788e5d1c784549f43259f7
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-03-14 12:36:49 +0700

    fix: lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,19 @@ const authPaths = ['/register', '/login', '/forgot-password']
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const sessionToken = request.cookies.get('sessionToken')?.value
+  const sessionToken = request.cookies.get('sessionToken')?.value;
 
   if (privatePaths.some((path) => pathname.startsWith(path)) && !sessionToken) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/login', request.url), { status: 307 })
   }
 
   if (authPaths.some((path) => pathname.startsWith(path)) && sessionToken) {
-    return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.redirect(new URL('/home', request.url), { status: 307 })
   }
   return NextResponse.next()
 }
 
 export const config = {
   runtime: 'nodejs',
-  matcher: [...privatePaths, ...authPaths]
+  matcher: ['/home', '/profile/:path*', '/login', '/register']
 }
